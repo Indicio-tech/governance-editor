abb2b6011571226a356250a65dbed850cfa3bed5
Eldersonar <slavaecu@gmail.com>
2022-10-18 19:14:12 -0600

    feat: ability to upload json file
    Signed-off-by: Eldersonar <slavaecu@gmail.com>

--- src/UI/Governance.js
+++ src/UI/Governance.js
@@ -1,26 +1,53 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react"
+import styled from "styled-components"
 
-const Input = styled.input``;
+const Input = styled.input``
 const Form = styled.form`
   overflow: hidden;
   margin-bottom: 10px;
-`;
-const SubmitFormBtn = styled.button``;
+`
+const SubmitFormBtn = styled.button``
 
 function Governance(props) {
+  const [selectedGovernance, setSelectedGovernance] = useState("trash")
+  // const [governanceFileName, setGovernanceFileName] = useState("Choose file");
+
+  let governanceSelectHandler = (event) => {
+    const file = event.target.files[0]
+
+    if (file) {
+      const fileReader = new FileReader()
+      fileReader.readAsText(event.target.files[0], "UTF-8")
+      fileReader.onload = (e) => {
+        // console.log("e.target.result", e.target.result);
+        setSelectedGovernance(e.target.result)
+      }
+
+      // setGovernanceFileName(event.target.files[0].name);
+    }
+  }
+
+  const handleLogoSubmit = async (e) => {
+    e.preventDefault()
+
+    console.log(selectedGovernance)
+    if (selectedGovernance) {
+    } else {
+    }
+  }
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleLogoSubmit}>
         <Input
           type="file"
           accept=".json"
-          // onChange={logoSelectHandler}
+          onChange={governanceSelectHandler}
         ></Input>
         <SubmitFormBtn type="submit">Upload</SubmitFormBtn>
       </Form>
     </>
-  );
+  )
 }
 
-export default Governance;
+export default Governance
